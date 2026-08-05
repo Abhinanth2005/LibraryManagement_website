ab383332084a4317d9a45d325ec78bb610f44671
Your Name <you@example.com>
2026-08-05 11:28:42 +0530

    Update Stripe success and cancel URLs for production

--- demo/src/services/api.js
+++ demo/src/services/api.js
@@ -1,25 +1,37 @@
+// import axios from "axios";
+
+// console.log(import.meta.env.VITE_API_URL);
+
+// const api = axios.create({
+//    baseURL: import.meta.env.VITE_API_URL,
+//     withCredentials: true,
+// });
+
+// function getCookie(name) {
+//     const value = `; ${document.cookie}`;
+//     const parts = value.split(`; ${name}=`);
+//     if (parts.length === 2) {
+//         return parts.pop().split(";").shift();
+//     }
+// }
+
+// console.log(import.meta.env.VITE_API_URL);
+
+// api.interceptors.request.use((config) => {
+//     config.headers["X-CSRFToken"] = getCookie("csrftoken");
+//     return config;
+// });
+
+// export default api;
+
 import axios from "axios";
 
-console.log(import.meta.env.VITE_API_URL);
-
 const api = axios.create({
-   baseURL: import.meta.env.VITE_API_URL,
-    withCredentials: true,
-});
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) {
-        return parts.pop().split(";").shift();
-    }
-}
-
-console.log(import.meta.env.VITE_API_URL);
-
-api.interceptors.request.use((config) => {
-    config.headers["X-CSRFToken"] = getCookie("csrftoken");
-    return config;
+  xsrfCookieName: "csrftoken",
+  xsrfHeaderName: "X-CSRFToken",
 });
 
 export default api;
