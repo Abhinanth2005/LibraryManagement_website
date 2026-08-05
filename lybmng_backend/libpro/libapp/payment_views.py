0f83b0a6434317c9ca029de8398c17e3e902599b
Your Name <you@example.com>
2026-08-05 10:22:12 +0530

    Update Stripe success and cancel URLs for production

--- lybmng_backend/libpro/libapp/payment_views.py
+++ lybmng_backend/libpro/libapp/payment_views.py
@@ -77,7 +77,7 @@ def create_checkout_session(request, book_id):
         )
 
 
-@api_view(["GET","POST"])
+@api_view(["POST"])
 # @permission_classes([IsAuthenticated])
 def payment_success(request):
 
